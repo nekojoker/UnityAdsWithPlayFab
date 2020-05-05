a1a5c8788e62ae745774f88111fe2de22a4f17dd
nekojoker <nkjtky1@gmail.com>
2020-05-05 15:59:35 +0900

    Initial commit

---
+++ Assets/Scripts/PlayFabController.cs
@@ -0,0 +1,30 @@
+﻿using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabController : MonoBehaviour
+{
+    void Start()
+    {
+        // Call login
+        PlayFabAuthService.Instance.Authenticate(Authtypes.Silent);
+    }
+
+    private void PlayFabAuthService_OnLoginSuccess(LoginResult success)
+    {
+        Debug.Log("Login Success!!");
+
+
+    }
+
+    private void OnEnable()
+    {
+        // Add login success event
+        PlayFabAuthService.OnLoginSuccess += PlayFabAuthService_OnLoginSuccess;
+    }
+
+    private void OnDisable()
+    {
+        // Remove login success event
+        PlayFabAuthService.OnLoginSuccess -= PlayFabAuthService_OnLoginSuccess;
+    }
+}
